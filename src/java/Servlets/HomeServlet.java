943202f41de0a93d29dcd3e19e9e4b307b2e4b2f
699785 <699785@SAIT225170IT.ACDM.DS.SAIT.CA>
2021-02-16 00:56:59 -0700

    Added the home/login pages The html code for the home/login page have been added and the project will now display the home page when run

--- src/java/Servlets/HomeServlet.java
+++ src/java/Servlets/HomeServlet.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Servlets;
 
 import java.io.IOException;
@@ -13,9 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 
 
 public class HomeServlet extends HttpServlet {
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
     }
 
    
--- web/WEB-INF/login.jsp
+++ web/WEB-INF/login.jsp
@@ -9,9 +9,15 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Login</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Login</h1>
+        
+        <form method="post" action="login">
+            <label>Username:</label><input type="text" name="username" value="">
+            <label>Password:</label><input type="password" name="password" value="">
+            <input type="buttton" value="submit" name="submit">
+        </form>
     </body>
 </html>
